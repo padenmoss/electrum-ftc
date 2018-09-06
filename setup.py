spesmilo/electrum

version: improve version reporting
Now, the version string is dynamically fetched from git for
user facing version strings on the command line and in the GUI.
Upstream's version string is still used for machine use such
that third party tools and apps are not confused.

setup.py
@@ -4,6 +4,7 @@
 
 from setuptools import setup
 from setuptools.command.install import install
+from setuptools.command.build_py import build_py
 from distutils import core
 import os
 import sys
@@ -35,6 +36,17 @@ if platform.system() in ['Linux', 'FreeBSD', 'DragonFly']:
     ]
 
 
+class BuildPyCommand(build_py):
+    def run(self):
+        build_py.run(self)
+        with open('build/lib/electrum_ftc/version.py', 'r+') as fp:
+            verfile = fp.readlines()
+            verfile[0] = "ELECTRUM_FTC_VERSION = '{}'\n".format(
+                version.ELECTRUM_FTC_VERSION)
+            fp.seek(0)
+            fp.writelines(verfile)
+            fp.truncate()
+
 class InstallCommand(install):
     def run(self):
         if platform.system() is 'Windows':
@@ -44,8 +56,8 @@ class InstallCommand(install):
 
 setup(
     name="Electrum-FTC",
-    version=version.ELECTRUM_VERSION,
-    cmdclass={'install': InstallCommand},
+    version=version.ELECTRUM_FTC_VERSION,
+    cmdclass={'install': InstallCommand, 'build_py': BuildPyCommand},
     install_requires=[
         'pyaes>=0.1a1',
         'ecdsa>=0.9',

padenmoss/electrum-ftc

setup.py
@@ -11,6 +11,7 @@ import subprocess
 
 from setuptools import setup, find_packages
 from setuptools.command.install import install
+from setuptools.command.build_py import build_py
 
 with open('contrib/requirements/requirements.txt') as f:
     requirements = f.read().splitlines()
@@ -67,10 +68,20 @@ class CustomInstallCommand(install):
             except Exception as e:
                 print('Warning: building icons file failed with {}'.format(e))
 
+class BuildPyCommand(build_py):
+    def run(self):
+        build_py.run(self)
+        with open('build/lib/electrum/version.py', 'r+') as fp:
+            verfile = fp.readlines()
+            verfile[0] = "ELECTRUM_FTC_VERSION = '{}'\n".format(
+                version.ELECTRUM_FTC_VERSION)
+            fp.seek(0)
+            fp.writelines(verfile)
+            fp.truncate()
 
 setup(
     name="Electrum",
-    version=version.ELECTRUM_VERSION,
+    version=version.ELECTRUM_FTC_VERSION,
     install_requires=requirements,
     extras_require=extras_require,
     packages=[
@@ -98,6 +109,7 @@ setup(
     url="https://electrum.org",
     long_description="""Lightweight Bitcoin Wallet""",
     cmdclass={
+        'build_py': BuildPyCommand,
         'install': CustomInstallCommand,
     },
 )